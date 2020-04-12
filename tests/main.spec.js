ba878a4c46f9bd13c82cbd5578f7ed196920b21a
Thiago Calura <[omitted]>
2020-04-12 17:12:25 -0300

    Aula 81 Metodos Chai

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,29 +1,37 @@
+var expect = require('chai').expect;
+
 describe('Main', function() {
-  describe('Method A', function() {
-    context('Case 1', function() {
-      it('should happen blabla', function() {
-        // espera que aconteça
-        // Entra de dados / método sum(2,2)
-        // Espera retornar (4) => true | (3) => false => broken test
-      });
-    });
+  var arr;
 
-    context('Case 2', function() {
-      it('should happen blabla', function() {
-        // espera que aconteça
-        // Entra de dados / método sum(2,2)
-        // Espera retornar (4) => true | (3) => false => broken test
-      });
-
-      it('should happen mimimi', function() {
-        // espera que aconteça
-        // Entra de dados / método sum(2,2)
-        // Espera retornar (4) => true | (3) => false => broken test
-      });
-    });
+  // roda todas as vezes, antes de CADA bloco
+  beforeEach(function() {
+    arr = [1, 2, 3];
   });
 
-  describe('Method B', function() {
+  // testar tipos ou se existe (smoke test)
+  it('should be an array', function() {
+    expect(arr).to.be.a('array');
+  });
 
+  it('should have a size of 4 when push another value to the array', function() {
+    arr.push(4);
+    // console.log(arr.length); // 4
+    expect(arr).to.have.lengthOf(4);
+  });
+
+  it('should remove the value 3 when use pop in the array', function() {
+    // console.log(arr.pop() === 3); // true
+    arr.pop();
+    expect(arr).to.not.include(3);
+  });
+
+  it('should return true when pop 3 from the array', function() {
+    expect(arr.pop() === 3).to.be.true;
+  });
+
+  it('should have a size of 2 when pop a value from the array', function() {
+    arr.pop();
+    console.log(arr.length); // 2
+    expect(arr).to.have.lengthOf(2);
   });
 });
