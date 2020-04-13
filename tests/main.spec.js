139b5ea29433b98109e71e2ab910f04d93105dcc
Thiago Calura <[omitted]>
2020-04-12 21:18:21 -0300

    Aula 84 Convertendo e testando em ES6

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,10 +1,11 @@
-var expect = require('chai').expect;
+import { expect } from 'chai';
+import { sum, sub, mult, div } from '../src/main';
 
 describe('Main', function() {
   var arr;
 
   // roda todas as vezes, antes de CADA bloco
-  beforeEach(function() {
+  beforeEach(() => {
     arr = [1, 2, 3];
   });
 
@@ -34,4 +35,69 @@ describe('Main', function() {
     console.log(arr.length); // 2
     expect(arr).to.have.lengthOf(2);
   });
+});
+
+describe('Calc', function() {
+
+  //smoke tests
+  // describe('Smike tests', function(){
+
+  //   it('should exist the calc lib', function(){
+  //     expect(calc).to.exist;
+  //   });
+
+  //   it('should exist the method `sum`', function(){
+  //     expect(calc.sum).to.exist;
+  //     expect(calc.sum).to.be.a('function'); // to.be.a.function; nao funcionou
+  //   });
+
+  //   it('should exist the method `sub`', function(){
+  //     expect(calc.sub).to.exist;
+  //     expect(calc.sub).to.be.a('function');
+  //   });
+
+  //   it('should exist the method `div`', function(){
+  //     expect(calc.div).to.exist;
+  //     expect(calc.div).to.be.a('function');
+  //   });
+
+  //   it('should exist the method `mult`', function(){
+  //     expect(calc.mult).to.exist;
+  //     expect(calc.mult).to.be.a('function');
+  //   });
+
+  // });
+
+  describe('Sum', function() {
+    it('should return 4 when `sum(2,2)`', function() {
+      expect(sum(2,2)).to.be.equal(4);
+    });
+  });
+
+  describe('Sub', function() {
+    it('should return 4 when `sub(6,2)`', function() {
+      expect(sub(6,2)).to.be.equal(4);
+    });
+
+    it('should return -4 when `sub(6,10)`', function() {
+      expect(sub(6,10)).to.be.equal(-4);
+    });
+  });
+
+  describe('Div', function() {
+    it('should return 3 when `div(6,2)`', function() {
+      expect(div(6,2)).to.be.equal(3);
+    });
+
+    it('should return `Não é possivel divisão por zero!` when divide by 0', function() {
+      expect(div(6,0)).to.be.equal('Não é possivel divisão por zero!');
+    });
+  });
+
+  describe('Mult', function() {
+    it('should return 12 when `mult(6,2)`', function() {
+      expect(mult(6,2)).to.be.equal(12);
+    });
+  });
+
 });
